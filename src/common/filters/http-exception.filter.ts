6f51fd117f26ea43a5aedd2593800da5f198da5b
akashk11 <akashvinod777@gmail.com>
2025-06-19 22:58:38 +0530

    feat(exception): implement global exception filter for handling HTTP errors

---
+++ src/common/filters/http-exception.filter.ts
@@ -0,0 +1,36 @@
+import {
+  ExceptionFilter,
+  Catch,
+  ArgumentsHost,
+  HttpException,
+  HttpStatus,
+} from '@nestjs/common';
+import { Response } from 'express';
+
+@Catch()
+export class GlobalExceptionFilter implements ExceptionFilter {
+  catch(exception: any, host: ArgumentsHost) {
+    const ctx = host.switchToHttp();
+    const response = ctx.getResponse<Response>();
+
+    console.log(exception);
+    const status =
+      exception instanceof HttpException
+        ? exception.getStatus()
+        : HttpStatus.INTERNAL_SERVER_ERROR;
+
+    const message =
+      exception instanceof HttpException
+        ? exception.getResponse()
+        : 'Internal server error';
+
+    response
+      .status(status)
+      .json({
+        statusCode: status,
+        timestamp: new Date().toISOString(),
+        path: ctx.getRequest().url,
+        message: typeof message === 'object' ? message : { message },
+      });
+  }
+}
