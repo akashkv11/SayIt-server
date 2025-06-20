d16a3d13bf2e490607dc9cc927ef2ba4eff8a667
akashk11 <akashvinod777@gmail.com>
2025-06-20 14:45:22 +0530

    refactor(main): reorganize imports and enhance CORS configuration

--- src/main.ts
+++ src/main.ts
@@ -1,20 +1,28 @@
-import { NestFactory } from '@nestjs/core';
-import * as dotenv from 'dotenv';
-import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
 import { GlobalExceptionFilter } from './common/filters/http-exception.filter';
+import { SuccessResponseInterceptor } from './common/interceptors/success-response.interceptor';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
-      forbidNonWhitelisted: true,
+      // forbidNonWhitelisted: true,
       transform: true,
     }),
   );
 
+  app.enableCors({
+    origin: 'http://localhost:5173', // <-- change this to your frontend origin
+    credentials: true,
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+  });
+
   app.useGlobalFilters(new GlobalExceptionFilter());
+  app.useGlobalInterceptors(new SuccessResponseInterceptor());
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
